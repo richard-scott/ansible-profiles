206706c98db568b65243b4ff5e18b254b7bc461d
Richard Scott <richard.scott@aeriandi.com>
2017-10-23 11:36:13 +0100

    added loggign to update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -4,7 +4,7 @@ echo -n "Updating GIT Repos"
 for GR in $(find ~ -name .git) ; do
   if [ $(find ${GR} -maxdepth 0 -type d -mmin +${REFRESH} | wc -l) -gt 0 ]; then
     cd $(dirname ${GR})
-    git pull --quiet >/dev/null 2>&1
+    git pull --quiet >.git-pull.log 2>&1
     echo -n "."
   fi
 done
