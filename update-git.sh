76e6fafa56f54bb4fc46612694b787a6a96ff1dc
Richard Scott <richard.scott@aeriandi.com>
2017-10-23 11:40:10 +0100

    updated notifications output

--- update-git.sh
+++ update-git.sh
@@ -3,6 +3,7 @@ REFRESH=3
 echo "Updating GIT Repos"
 for GR in $(find ~ -name .git) ; do
   if [ $(find ${GR} -maxdepth 0 -type d -mmin +${REFRESH} | wc -l) -gt 0 ]; then
+    echo "Repo path: ${G}"
     cd $(dirname ${GR})
     git pull
   fi
