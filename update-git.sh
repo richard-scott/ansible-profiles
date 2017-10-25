12a64f02c7907ba493a581695795a55ef433947e
Richard Scott <richard.scott@aeriandi.com>
2017-10-25 10:09:05 +0100

    updated script

--- update-git.sh
+++ update-git.sh
@@ -4,7 +4,7 @@ cd $(dirname ${0})
 echo "  >> $(pwd)"
 git pull | egrep -v "^Already up-to-date.$"
 cd
-REFRESH=0
+REFRESH=5
 for GR in $(find ~ -name .git) ; do
   if [ $(find ${GR} -maxdepth 0 -type d -mmin +${REFRESH} | wc -l) -gt 0 ]; then
     echo "  >> $(dirname ${GR})"
