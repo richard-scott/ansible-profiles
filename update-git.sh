f351425339a91ac923c042e5f205d30073171818
Richard Scott <richard@pointb.co.uk>
2017-10-25 10:03:11 +0100

    tweaked script

--- update-git.sh
+++ update-git.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+echo "Updating GIT Repos"
 cd $(dirname ${0})
+echo "Repo path: $(pwd)"
 git pull | egrep -v "^Already up-to-date.$"
 cd
 REFRESH=5
-echo "Updating GIT Repos"
 for GR in $(find ~ -name .git) ; do
   if [ $(find ${GR} -maxdepth 0 -type d -mmin +${REFRESH} | wc -l) -gt 0 ]; then
     echo "Repo path: ${GR}" | sed 's|/.git$//'
