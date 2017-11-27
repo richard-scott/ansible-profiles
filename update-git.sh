0b92cfdd8fb7992dd7e96b79e548d8c6646d9178
Richard Scott <richard.scott@aeriandi.com>
2017-11-27 10:21:51 +0000

    updated output status

--- update-git.sh
+++ update-git.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Checking GIT Repos:"
 cd $(dirname ${0})
-echo "  >> $(pwd)"
 if [[ $(git remote -v | wc -l) -gt 0 ]]; then
+  echo "  >> $(pwd)"
   git pull | egrep -v "^Already up-to-date.$"
 fi
 cd
 REFRESH=${1:-5}
 for GR in $(find ~ -name .git) ; do
   if [ $(find ${GR} -maxdepth 0 -type d -mmin +${REFRESH} | wc -l) -gt 0 ]; then
-    echo "  >> $(dirname ${GR})"
     cd $(dirname ${GR})
     if [[ $(git remote -v | wc -l) -gt 0 ]]; then
+      echo "  >> $(dirname ${GR})"
       git pull | egrep -v "^Already up-to-date.$"
     fi
   fi
