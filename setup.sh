b8abca7bd74447f6f02bfe36e3697cc5eebd49eb
Richard Scott <richard@pointb.co.uk>
2017-12-13 09:52:13 +0000

    updated tabs

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [[ ! -d ~/.aws ]]; then
-  echo "Unable to find ~/.aws, it doesn't look like you have configured any profiles yet!"
-  echo "Please do that and the run this script to add the wrapper hooks"
-  exit 1
+	echo "Unable to find ~/.aws, it doesn't look like you have configured any profiles yet!"
+	echo "Please do that and the run this script to add the wrapper hooks"
+	exit 1
 fi
 if ! grep -q Ansible/bin ~/.bashrc ; then
-  cd ~
+	cd ~
 	git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 
 	echo "Adding hooks to ~/.bashrc to enable use custom 'ansible' wrapper to generate aliases"
@@ -22,7 +22,7 @@ if ! grep -q Ansible/bin ~/.bashrc ; then
 	export PATH="${HOME}/Ansible/bin:${PATH}"
 	cd ~/Ansible/bin/update-git.sh
 	cd ~
-  echo "Loading Alias list for:"
+	echo "Loading Alias list for:"
 	for PROFILE in $(ansible list-profiles) ; do
 	  echo "  >> ${PROFILE}"
 	  eval "$(ansible ${PROFILE} list instances aliases)"
